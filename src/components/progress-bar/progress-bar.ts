c18089990a8150fd0d0e8c0b09cd39a3acac8205
kevinjulio93 <kevinjulio93@outlook.com>
2018-02-08 14:50:56 -0500

    progress bar ok

--- src/components/progress-bar/progress-bar.ts
+++ src/components/progress-bar/progress-bar.ts
@@ -6,8 +6,10 @@ import { Component, Input } from '@angular/core';
 })
 export class ProgressBarComponent {
 
+  
   @Input() color: string = '#5CBCB9';
   @Input() progress: number = 0;
+  @Input() Min_progress: number = 20;
   
 
   constructor() {}
